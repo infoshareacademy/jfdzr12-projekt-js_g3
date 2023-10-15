a15bfe1480e690aa59ba579246522562bfd14dca
Marta Cichacka <martacichacka713@gmail.com>
2023-10-15 21:13:20 +0200

    Zadanie3 added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+//Zadanie1
 // const sentense = "Pierwszy #projekt #javascript"
-
 // const words = sentense.split(" ");
 // for (let i=0; i < words.length; i++) {
 //     if (words[i].charAt(0) === "#") {
@@ -8,10 +8,36 @@
 //     }
 // }
 
-let word = "kebab-case";
-const re = /-|_/;
-let result = word.split(re);
-for(let i=0; i < result.length; i++) {
-    result[i] = result[i].charAt(0).toUpperCase() + result[i].substring(1);
+//Zadanie2
+// let word = "kebab-case";
+// const re = /-|_/;
+// let result = word.split(re);
+// for(let i=0; i < result.length; i++) {
+//     result[i] = result[i].charAt(0).toUpperCase() + result[i].substring(1);
+// }
+// console.log(result.join(""));
+
+//Zadanie3
+const passwordLength = 12;
+const minOccurence = 1;
+const characters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz", "0123456789", "!#$%&()*+,-./:;<=>?@[^_`{|}"];
+let result = "";
+let listofSigns = "";
+function randomSign(list){
+    return list[Math.floor( Math.random() *  list.length)];
 }
-console.log(result.join(""));
+
+for (let j = 0; j < 4; j++){
+    for (let k = 0; k < minOccurence; k++){
+    listofSigns = listofSigns + randomSign(characters[j]);
+    }
+}
+while (listofSigns.length <= passwordLength) {
+    listofSigns = listofSigns + randomSign(characters.join());
+}
+for (let m = passwordLength; m > 0; m--) {
+    rand = randomSign(listofSigns);
+    result = result + rand;
+    listofSigns = listofSigns.replace(rand, "");
+}
+console.log(result);
