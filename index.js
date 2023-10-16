dc88cbf37a9ab4161f015bdfce853f695a5fa3b1
Marta Cichacka <martacichacka713@gmail.com>
2023-10-16 17:38:46 +0200

    Zadanie 2 modified- no modification check added

--- index.js
+++ index.js
@@ -9,34 +9,37 @@
 // }
 
 //Zadanie2
-// let word = "kebab-case";
-// const re = /-|_/;
-// let result = word.split(re);
-// for(let i=0; i < result.length; i++) {
-//     result[i] = result[i].charAt(0).toUpperCase() + result[i].substring(1);
-// }
-// console.log(result.join(""));
-
+let word = "KebabCase";
+const re = /-|_/;
+let result = word.split(re);
+if (result.length === 1 && result[0].charAt(0).toUpperCase() === result[0].charAt(0)) {
+    console.log(`${word} - no modification needed`)
+} else {
+for(let i=0; i < result.length; i++) {
+    result[i] = result[i].charAt(0).toUpperCase() + result[i].substring(1);
+}
+console.log(result.join(""));
+}
 //Zadanie3
-const passwordLength = 12;
-const minOccurence = 1;
-const characters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz", "0123456789", "!#$%&()*+,-./:;<=>?@[^_`{|}"];
-let result = "";
-let listofSigns = "";
-function randomSign(list){
-    return list[Math.floor( Math.random() *  list.length)];
-}
-for (let j = 0; j < 4; j++){
-    for (let k = 0; k < minOccurence; k++){
-    listofSigns = listofSigns + randomSign(characters[j]);
-    }
-}
-while (listofSigns.length <= passwordLength) {
-    listofSigns = listofSigns + randomSign(characters.join("")); 
-}
-for (let m = passwordLength; m > 0; m--) {
-    rand = randomSign(listofSigns);
-    result = result + rand;
-    listofSigns = listofSigns.replace(rand, "");
-}
-console.log(result);
+// const passwordLength = 12;
+// const minOccurence = 1;
+// const characters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz", "0123456789", "!#$%&()*+,-./:;<=>?@[^_`{|}"];
+// let result = "";
+// let listofSigns = "";
+// function randomSign(list){
+//     return list[Math.floor( Math.random() *  list.length)];
+// }
+// for (let j = 0; j < 4; j++){
+//     for (let k = 0; k < minOccurence; k++){
+//     listofSigns = listofSigns + randomSign(characters[j]);
+//     }
+// }
+// while (listofSigns.length <= passwordLength) {
+//     listofSigns = listofSigns + randomSign(characters.join("")); 
+// }
+// for (let m = passwordLength; m > 0; m--) {
+//     rand = randomSign(listofSigns);
+//     result = result + rand;
+//     listofSigns = listofSigns.replace(rand, "");
+// }
+// console.log(result);
